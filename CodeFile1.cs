c2958dbc1b50b0ea12db2058468202e97ea4f79a
fr0zy777 <maxpaliy1@gmail.com>
2023-05-05 18:52:29 +0300

    Laby

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace LabirintMax
+{
+    static class Consts
+    {
+        public const int LabirintHeight = 30;
+        public const int LabirintWidth = 30;
+
+        public const char PersonSymbol = 'A';
+
+        public const int maxHp = 200;
+    }
+}
